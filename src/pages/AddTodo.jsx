c57163994ff3d9da61f252c28bc1a12d93518dab
Jhacy456 <jakpesad20@gmail.com>
2024-10-03 15:50:10 +0000

    Added todo

--- src/pages/AddTodo.jsx
+++ src/pages/AddTodo.jsx
@@ -1,11 +1,17 @@
 import { useNavigate } from "react-router-dom";
+import axios, { toFormData } from "axios";
 
 const AddTodos = () => {
     const navigate = useNavigate();
 
-    const saveTodo = (event) => {
+    const saveTodo = async (event) => {
         event.preventDefault();
+        // collect form input
+        const formData = new FormData(event.target)
         // Post data to todo api
+          await axios.post("https://todoapi-9uev.onrender.com/todos", {
+            title: formData.get('todo')
+          })
         // Goto the homepage
         navigate('/');
 
@@ -15,7 +21,7 @@ const AddTodos = () => {
         <div>
             <h1>Add A New Todo</h1>
             <form onSubmit={saveTodo}>
-                <input type="Text" placeholder="Enter Todo" required />
+                <input type="Text" placeholder="Enter Todo" required name="todo" /> 
                 <button type="submit">Submit</button>
             </form>
         </div>
--- src/pages/Todos.jsx
+++ src/pages/Todos.jsx
@@ -1,16 +1,31 @@
+import { useEffect, useState } from "react";
 import TodoTile from "../components/TodoTile";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 const Todos = () => {
+    // 1.Declare state to store todos
+    const [todos, setTodos] = useState([]);
+    // 2. Define a function to get todos
+    const getTodos = async () => {
+        // Use axios to get todos
+        const response = await axios.get('https://todoapi-9uev.onrender.com/todos');
+        // Update todos state
+        setTodos(response.data);
+    }
+    // 3. Call function with useEffect
+    useEffect(() =>{
+        getTodos();
+    },[])
+
     return (
         <div>
-            
+
             <div>
-            <h1>All Todos</h1>
-            <TodoTile title={'Creating a React App'}/>
-            <TodoTile title={'Install Tailwind CSS'}/>
-            <TodoTile title={'Commit code to Github'}/>
-            <TodoTile title={'Host it on Netlify'}/>
+                <h1>All Todos</h1>
+                    {todos.map((todo) =>{
+                        return <TodoTile title={todo.title} key={todo._id}/>
+                    })}
             </div>
             <Link to={'/add'}>Add Todo</Link>
         </div>
